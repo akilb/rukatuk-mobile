7e6c0f8b0584c1fb6f7e5752f702bad4fba564ba
Akil Burgess <akilburgess@gmail.com>
2017-06-24 00:19:28 +0100

    Add venue map to details

--- app/screens/event-detail/EventDetailScreen.js
+++ app/screens/event-detail/EventDetailScreen.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import Moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import MapView from 'react-native-maps';
 
 import appStyles, { theme, navigatorStyle } from '../../config/styles';
 import styles from './styles';
@@ -92,7 +93,46 @@ export default class EventDetailScreen extends Component {
             Learn more
           </Text>
         </View>
+
+        <View style={appStyles.card}>
+          <MapView
+            style={{
+              height: 200
+            }}
+            initialRegion={{
+              latitude: Number.parseFloat(event.venue.latitude),
+              longitude: Number.parseFloat(event.venue.longitude),
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.010,
+            }}
+            zoomEnabled={false}
+            rotateEnabled={false}
+            scrollEnabled={false}
+            pitchEnabled={false}
+          />
+          <View style={{ padding: 10 }}>
+            <Text style={[
+              appStyles.p,
+              {
+                fontWeight: 'bold'
+              }
+            ]}>
+              {event.venue.name}
+            </Text>
+            <Text style={appStyles.p}>
+              {event.venue.addressLine1}
+            </Text>
+            <Text style={appStyles.p}>
+              {event.venue.city}, {event.venue.postalCode}
+            </Text>
+            <Text style={appStyles.p}>
+              {event.venue.country}
+            </Text>
+          </View>
+        </View>
       </ScrollView>
     );
+
+    // {"city":"London","country":"United Kingdom","postalCode":"SE10 0DX","longitude":"0.003664100000037251","addressLine1":"The O2","latitude":"51.5025248","addressLine2":"Greenwich","name":"All Bar One O2"}
   }
 }
