8fe2eddf2479d9c08288c5c850bc12c075b5d91c
Akil Burgess <akilburgess@gmail.com>
2017-08-14 00:53:14 +0100

    Ruk-A-Tuk API now lives at `api.rukatuk.com`

--- app/config/settings.js
+++ app/config/settings.js
@@ -4,7 +4,7 @@ const settings = {
   facebookUrl: 'https://facebook.com/rukatuk',
   instagramUrl: 'https://instagram.com/rukatuk',
   twitterUrl: 'https://twitter.com/rukatuk',
-  rukatukApiUrl: 'http://rukatuk-api.azurewebsites.net',
+  rukatukApiUrl: 'https://api.rukatuk.com',
   googleAnalyticsAccount: Config.GOOGLE_ANALYTICS_ACCOUNT
 };
 
